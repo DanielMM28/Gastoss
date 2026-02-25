b06ba7f6e2dcaa3ae2502b19dffa8f096d9250d8
DanielMM28 <danieldavidmu9@gmail.com>
2026-02-25 15:30:23 -0500

    Fix case-sensitive CSS import for production build

--- src/presupuesto/formularios/formulario.jsx
+++ src/presupuesto/formularios/formulario.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import { supabase } from "../../supabaseclient"
-import "../../app.css"
+import "../../App.css"
 
 function Formulario({ mostrar, cerrar, gastoEditar, recargar }) {
   const [titulo, setTitulo] = useState("")
