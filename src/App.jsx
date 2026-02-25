83274037e286b3c241ff73223089c0f1be9f14c2
DanielMM28 <danieldavidmu9@gmail.com>
2026-02-24 22:15:16 -0500

    Diseño responsive agregado al dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { Routes, Route } from "react-router-dom"
 import Listado from "./listado"
 import Formulario from "./formulario"
 import Tableros from "./tablero"
-import Ingresos from "./ingresos"
+import ListadoIngresos from "./foringresos"
+
 
 function App() {
   return (
@@ -10,7 +11,10 @@ function App() {
   <Route path="/" element={<Listado />} />
   <Route path="/tablero/:id" element={<Tableros />} />
   <Route path="/crear" element={<Formulario />} />
-  <Route path="/ingresos" element={<Ingresos />} />
+
+  <Route path="/editar/:id" element={<Formulario />} />
+  <Route path="/ingresos/:id" element={<ListadoIngresos />} />
+    
 </Routes>
   )
 }
